7018b22fe1dac5fdacfccc96c737ec06b7b653bb
Kraxner Thomas <thomas.kraxner@illwerkevkw.at>
2023-01-13 13:59:16 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# About
+
+When using `DependencyTrack` to find nugets with critical CVE, there is no way to track down the inclusion path of the nuget introducing the vulnerability.
+After several hours of internet research without success I decided to write this.
+
+# Prerequisite
+
+* `node.js` - Installguide see https://nodejs.org/en/download/package-manager/
+* `nuget-deps-tree` - npm see https://www.npmjs.com/package/nuget-deps-tree
+	* `npm install -g nuget-deps-tree`
+
+# How does it work
+
+It calls `nuget-deps-tree` stores the resulting dependency tree. Then it finds the given nuget dependencies. To do so it traverses the dependencies and logs the up-path to shell, when the desired nuget was found.
+
+# Runit
+
+* (There must be a `dotnet build` or at least a `dotnet restore` to update the local nuget caches)
+* Don't forget to unblock the `nailDownNuget.ps1`
+* Run it in powershell `nailDownNuget.ps1 [path to sln] [name of the nuget] [version of the nuget]`
---
+++ nailDownNuget.ps1
@@ -0,0 +1,56 @@
+$slnPath=$args[0]
+$nugetName=$args[1]
+$nugetVersion=$args[2]
+
+function Find-Nuget 
+{
+    param (
+        [string]$path,
+        [string]$currentElementName,
+        [string]$currentElementVersion,
+        [string]$nugetName,
+        [string]$nugetVersion,
+        [object[]]$dependencies
+    )
+
+    $path = "$($path) > $($currentElementName)"
+    
+    if($currentElementName.Equals($nugetName) -and $currentElementVersion.Equals($nugetVersion))
+    {
+        Write-Host $path
+    }
+
+    if ($dependencies.Count -eq 0)
+    {
+        return
+    }
+    else 
+    {
+        foreach ($dependency in $dependencies)
+        {
+            Find-Nuget $path $dependency.id $dependency.version $nugetName $nugetVersion $dependency.dependencies
+        }
+    }
+}
+
+function Get-NugetPath 
+{
+    param (
+        [string]$slnPath,
+        [string]$nugetName,
+        [string]$nugetVersion
+    )
+
+    Write-Host "Executing nuget-deps-tree for path: '$($slnPath)'"
+    $dependencies = nuget-deps-tree $slnPath 2>$null #ignore stderr from nuget-deps-tree
+
+    $dependencyTree = $dependencies | ConvertFrom-Json
+
+    foreach ($project in $dependencyTree.projects)
+    {
+        Write-Host "Checking '$($project.name)'"
+        Find-Nuget "" $project.name "" $nugetName $nugetVersion $project.dependencies
+    }
+}
+
+Get-NugetPath $slnPath $nugetName $nugetVersion
